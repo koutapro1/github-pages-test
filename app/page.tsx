9f42181c4955b217e53b87eb57220e632b5746fb
tohatakota <tohatakota@team-lab.com>
2024-06-16 22:20:16 +0900

    ビルド

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,25 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
-  return <h1>test</h1>;
+import React, { useState, useEffect } from "react";
+
+export default function Page() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://uiux-up-specialist.team-lab.com/api/v1/messages"
+      );
+
+      const result = await response.json();
+      setData(result);
+    };
+
+    fetchData().catch((e) => {
+      // handle the error as needed
+      console.error("An error occurred while fetching the data: ", e);
+    });
+  }, []);
+
+  return <p>{data ? `Your data: ${data}` : "Loading..."}</p>;
 }
